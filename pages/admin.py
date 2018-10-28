50f6384c0a6ef923b1fae9ef69bbcbd892b81656
Kasimov Nuriddin <n.kasimov@outlook.com>
2018-10-28 23:37:22 +0100

    Added Contact to Admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from pages.models import Creation, About
+from pages.models import About, Contact, Creation
 
 # Register your models here.
-admin.site.register(Creation)
 admin.site.register(About)
+admin.site.register(Contact)
+admin.site.register(Creation)
