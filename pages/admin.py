945c9c2701226fa3cb9e67a562b0866de94e5880
Kasimov Nuriddin <n.kasimov@outlook.com>
2018-10-29 01:15:35 +0100

    Readibility enchancement on Contact Panel

--- pages/admin.py
+++ pages/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from pages.models import About, Contact, Creation
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('sent_at', 'email', 'message')
+
+
 admin.site.register(About)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(Creation)
